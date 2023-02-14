d0ba7f4ec8cfdbfa139680c4f6f7e090a81f787d
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-14 17:29:12 -0500

    deleted express app, storing scraped json data in fs

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,12 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import { getSalaryCapData } from "./webscraper";
-// import react, { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  const team = getSalaryCapData();
-  console.log(typeof team);
+export default async function Home() {
   return (
     <main className={styles.main}>
       <h1>NBA Front Office</h1>
-      <h1>{}</h1>
     </main>
   );
 }
