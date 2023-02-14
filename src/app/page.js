560e936b482257b98ea95e3afca6e0881c50ffbf
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-14 18:25:33 -0500

    team data can now display on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import teamSalaryCap from "teamSalaryCap.json";
 
 const inter = Inter({ subsets: ["latin"] });
 export default async function Home() {
+  console.log(
+    Object.values(teamSalaryCap).map((currentTeam) => {
+      return currentTeam.name;
+    })
+  );
+
+  const teams = Object.values(teamSalaryCap);
+  console.log(teams);
   return (
     <main className={styles.main}>
       <h1>NBA Front Office</h1>
+      {}
     </main>
   );
 }
