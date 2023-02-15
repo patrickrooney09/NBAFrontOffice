2ebe55a98d6d878c93ba41597b24d14a6886f04a
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-15 14:44:46 -0500

    added folders for salary data, redefined web scrapers, added json files for 2021 and 2020 season

---
+++ webscraper/playerwebscraper.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const url = "https://www.spotrac.com/nba/rankings";
+
+let allPlayers = {};
+
+axios(url).then((response) => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+  console.log(html);
+  // $(".xs-hide").each(function (i, ele) {
+  //   const title = $(ele).text();
+
+  //   // console.log("TITLE:", title);
+  //   // console.log("INDEX:", i);
+  //   allTeams[i] = { name: title };
+  // });
+
+  // let count = 0;
+  // let currentTeam = 0;
+  // $("td.center").each(function (i, ele) {
+  //   const title = $(ele).text();
+  //   if (count === 0) allTeams[currentTeam]["rank"] = title;
+  //   if (count === 1) allTeams[currentTeam]["winPercentage"] = title;
+  //   if (count === 2) allTeams[currentTeam]["signedPlayers"] = title;
+  //   if (count === 3) allTeams[currentTeam]["avgAge"] = title;
+  //   if (count === 4) allTeams[currentTeam]["activeCap"] = title;
+  //   if (count === 5) allTeams[currentTeam]["activeCapTop3"] = title;
+  //   if (count === 6) allTeams[currentTeam]["deadCap"] = title;
+  //   if (count === 7) allTeams[currentTeam]["totalCap"] = title;
+  //   if (count === 8) allTeams[currentTeam]["capSpace"] = title;
+  //   // if (count === 9)
+  //   //   allTeams[currentTeam]["projectedPracticalCapSpace"] = title;
+  //   if (count === 9) allTeams[currentTeam]["hardCap"] = title;
+  //   if (count === 9) {
+  //     count = 0;
+  //     currentTeam++;
+  //   } else {
+  //     count++;
+  //   }
+  // });
+
+  // fs.writeFile(
+  //   "salaryData/teamSalaries/2020teamSalaryCap.json",
+  //   JSON.stringify(allTeams),
+  //   (err) => {
+  //     if (err) throw err;
+  //     console.log("file successfully saved");
+  //   }
+  // );
+});
