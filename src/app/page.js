22c2bdcb9ba40157c0c1ba383ed7bca0796635ae
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-16 16:36:03 -0500

    renamed files

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import teamSalaryCap from "teamSalaryCap.json";
+import teamSalaryCap2022 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2022.json";
 
 const inter = Inter({ subsets: ["latin"] });
 export default async function Home() {
-  const teams = Object.values(teamSalaryCap);
+  const teams = Object.values(teamSalaryCap2022);
 
   return (
     <main className={styles.main}>
