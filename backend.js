02a3d0b5d431fd4f6b2d81e195fdc4038dbf10a3
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-16 18:22:01 -0500

    created backend to secure api key, data now showing up on front ends console logs

--- backend.js
+++ backend.js
@@ -7,8 +7,24 @@ require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json("hi");
+app.get("/teamStats", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.mysportsfeeds.com/v2.1/pull/nba/current/team_stats_totals.json",
+    auth: {
+      //http basic auth
+      username: process.env.MY_SPORTS_FEEDS_API,
+      password: "MYSPORTSFEEDS",
+    },
+  };
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 app.listen(8000, () => {
