8555ecfdf174d466c0f394e9c8f2ff913aff0351
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-17 16:05:00 -0500

    able to get google image as a point on graph

--- src/app/chart.js
+++ src/app/chart.js
@@ -7,8 +7,25 @@ import {
   Legend,
 } from "chart.js";
 import { Bubble } from "react-chartjs-2";
+import teamSalaryCap2022 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2022.json";
+import logo from "/Users/patrickrooney/NBAFrontOffice/salaryData/logos/nba-san-antonio-spurs-logo.png";
 
 ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
+
+// console.log(spurs);
+// console.log(teamSalaryCap2022);
+// let spurs = (
+//   <Image
+//     src="/Users/patrickrooney/NBAFrontOffice/salaryData/logos/nba-san-antonio-spurs-logo.png"
+//     alt="spurs"
+//     width="50"
+//     height="50"
+//   />
+// );
+
+// var Image =  n
+let spurs = new Image(50, 50);
+spurs.src = "https://www.google.com/images/logo.png";
 
 export const options = {
   scales: {
@@ -16,28 +33,41 @@ export const options = {
       beginAtZero: true,
     },
   },
+  elements: {
+    point: {
+      pointStyle: spurs,
+    },
+  },
 };
 
 export const data = {
   datasets: [
     {
-      label: "Red dataset",
-      data: Array.from({ length: 50 }, () => ({
-        x: Math.floor(Math.random() * 100) + -100,
-        y: Math.floor(Math.random() * 100) + -100,
-        r: Math.floor(Math.random() * 20) + 5,
-      })),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      label: "San Antonio Spurs",
+
+      data: [{ x: 0.241, y: 98691331, r: 50 }],
+      // pointStyle: spurs,
+
+      // backgroundImage: "url(salaryData/logos/nba-san-antonio-spurs-logo.png)",
     },
-    {
-      label: "Blue dataset",
-      data: Array.from({ length: 50 }, () => ({
-        x: Math.floor(Math.random() * 100) + -100,
-        y: Math.floor(Math.random() * 100) + -100,
-        r: Math.floor(Math.random() * 20) + 5,
-      })),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
+    // {
+    //   label: "Red dataset",
+    //   data: Array.from({ length: 50 }, () => ({
+    //     x: Math.floor(Math.random() * 100) + 0,
+    //     y: Math.floor(Math.random() * 100) + 0,
+    //     r: Math.floor(Math.random() * 20) + 5,
+    //   })),
+    //   backgroundColor: "rgba(255, 99, 132, 0.5)",
+    // },
+    // {
+    //   label: "Blue dataset",
+    //   data: Array.from({ length: 50 }, () => ({
+    //     x: Math.floor(Math.random() * 100) + -100,
+    //     y: Math.floor(Math.random() * 100) + -120,
+    //     r: Math.floor(Math.random() * 20) + 5,
+    //   })),
+    //   backgroundColor: "rgba(53, 162, 235, 0.5)",
+    // },
   ],
 };
 
