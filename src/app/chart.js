883eb1b147041eaa563bec1b928149364e9f7138
Patrick Rooney <patrickrooney09@gmail.com>
2023-02-20 10:41:59 -0500

    state now updating when adjusting years on dropdown menu

--- src/app/chart.js
+++ src/app/chart.js
@@ -11,109 +11,114 @@ import { withRouter } from "next/router";
 import { Bubble } from "react-chartjs-2";
 import teamSalaryCap2020 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2020.json";
 import teamSalaryCap2021 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2021.json";
-import teamSalaryCap2022 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2020.json";
+import teamSalaryCap2022 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2022.json";
 
 import { Dropdown } from "@nextui-org/react";
 
-ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
-
-const years = [
-  { key: "2020", name: "2020" },
-  { key: "2021", name: "2021" },
-  { key: "2022", name: "2022" },
-];
-
-const teams = Object.values(teamSalaryCap2022);
-const teamData = teams.map((currentTeam, index) => {
-  let logo = "circle";
-  if (currentTeam.logo) {
-    logo = new Image(50, 50);
-    logo.src = currentTeam.logo;
-  }
-
-  return {
-    label: currentTeam.name,
-    data: [
-      {
-        x: Number(
-          currentTeam.totalCap
-            .split("")
-            .filter((currentElement) => {
-              if (!isNaN(currentElement)) {
-                return currentElement;
-              }
-            })
-            .join("")
-        ),
-        y: Number(currentTeam.winPercentage),
-        r: 10,
-      },
-    ],
-    pointStyle: logo,
-  };
-});
-
-export const options = {
-  color: "white",
-  borderColor: "white",
-
-  scales: {
-    y: {
-      grid: {
-        color: "white",
-      },
-      title: {
-        color: "white",
-        display: true,
-        text: "Win Percentage",
-      },
-      beginAtZero: true,
-      ticks: {
-        color: "white",
-        // Include a dollar sign in the ticks
-        callback: function (value, index, ticks) {
-          return "%" + value;
-        },
-      },
-    },
-    x: {
-      grid: {
-        color: "white",
-      },
-      title: {
-        color: "white",
-        display: true,
-        text: "Salary Cap",
-      },
-      ticks: {
-        color: "white",
-        // Include a dollar sign in the ticks
-        callback: function (value, index, ticks) {
-          return "$" + value;
-        },
-      },
-    },
-  },
-  plugins: {
-    legend: {
-      position: "left",
-      labels: {
-        // usePointStyle: true, // logos
-
-        usePointStyle: false,
-        pointStyleWidth: 10,
-        padding: 5,
-      },
-    },
-  },
-};
-
-export const data = {
-  datasets: teamData,
-};
-
 export function Chart(props) {
-  console.log(props.year);
+  ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
+
+  let teams;
+  const getTeams = (year) => {
+    if (year === 2020) {
+      teams = Object.values(teamSalaryCap2020);
+    }
+    if (year === 2021) {
+      teams = Object.values(teamSalaryCap2021);
+    }
+    if (year === 2022) {
+      teams = Object.values(teamSalaryCap2022);
+    }
+  };
+  getTeams(props.year);
+
+  const teamData = teams.map((currentTeam, index) => {
+    let logo = "circle";
+    if (currentTeam.logo) {
+      logo = new Image(50, 50);
+      logo.src = currentTeam.logo;
+    }
+
+    return {
+      label: currentTeam.name,
+      data: [
+        {
+          x: Number(
+            currentTeam.totalCap
+              .split("")
+              .filter((currentElement) => {
+                if (!isNaN(currentElement)) {
+                  return currentElement;
+                }
+              })
+              .join("")
+          ),
+          y: Number(currentTeam.winPercentage),
+          r: 10,
+        },
+      ],
+      pointStyle: logo,
+    };
+  });
+
+  const options = {
+    color: "white",
+    borderColor: "white",
+
+    scales: {
+      y: {
+        grid: {
+          color: "white",
+        },
+        title: {
+          color: "white",
+          display: true,
+          text: `${props.year} Win Percentage`,
+        },
+        beginAtZero: true,
+        ticks: {
+          color: "white",
+          // Include a dollar sign in the ticks
+          callback: function (value, index, ticks) {
+            return "%" + value;
+          },
+        },
+      },
+      x: {
+        grid: {
+          color: "white",
+        },
+        title: {
+          color: "white",
+          display: true,
+          text: "Salary Cap",
+        },
+        ticks: {
+          color: "white",
+          // Include a dollar sign in the ticks
+          callback: function (value, index, ticks) {
+            return "$" + value;
+          },
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        position: "left",
+        labels: {
+          // usePointStyle: true, // logos
+
+          usePointStyle: false,
+          pointStyleWidth: 10,
+          padding: 5,
+        },
+      },
+    },
+  };
+
+  const data = {
+    datasets: teamData,
+  };
   return (
     <>
       <Bubble options={options} data={data} />
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import { useTheme, Text, Dropdown } from "@nextui-org/react";
 import styles from "./page.module.css";
 import teamSalaryCap2022 from "/Users/patrickrooney/NBAFrontOffice/salaryData/teamSalaries/teamSalaryCap2022.json";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { Chart } from "./chart";
 import logo from "/Users/patrickrooney/NBAFrontOffice/salaryData/logos/nba-san-antonio-spurs-logo.png";
 
@@ -21,7 +21,14 @@ export default function Home() {
   // axios.get("http://localhost:8000/teamStats").then((response) => {
   //   console.log(response.data);
   // });
+  const [selected, setSelected] = useState(2020);
 
+  const handleChange = async (event) => {
+    event.preventDefault();
+
+    setSelected(Number(event.target.value));
+    console.log(selected);
+  };
   return (
     <main
       className={styles.main}
@@ -32,8 +39,16 @@ export default function Home() {
       }}
     >
       <h1>NBA Front Office</h1>
-      <Chart class="chart" year={2020} />
-
+      <Chart class="chart" year={selected} />
+      <form id="sort-campus-form">
+        <label htmlFor="sorting-campuses">Sort By: </label>
+        <select name="sorting-campuses" onChange={handleChange}>
+          <option value="">-</option>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+        </select>
+      </form>
       {/* <Image src={logo} alt="spurs" width="50" height="50" /> */}
       {/* <ol>
         {teams.map((currentTeam) => {
